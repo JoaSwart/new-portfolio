dfaa30f38975c12f0af796b0b3c1c733ef3dbf72
JoaSwart <leentjezz@outlook.com>
2025-10-07 13:39:37 +0200

    added email form and selfie

--- script.js
+++ script.js
@@ -219,3 +219,13 @@ if (contactStarsContainer) createStars(contactStarsContainer, 80); // 80 sterren
     }
 
 });
+
+function sendEmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_ym4idag", "template_ho3gamc",parms).then(alert("Email Sent!"));
+}
